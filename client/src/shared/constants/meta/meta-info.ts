55b44266eaf25e212a8ace48fff55b1802ffc5d7
donottookey <kirill123iop@gmail.com>
2026-06-05 16:12:50 +0500

    fix meta

--- client/src/shared/constants/meta/meta-info.ts
+++ client/src/shared/constants/meta/meta-info.ts
@@ -2,64 +2,47 @@ import { Description } from "@radix-ui/react-dialog";
 
 export const META_INFO = {
   cover: {
-    ru: '',
-    kk: '',
-    en: '',
+    ru: "",
+    kk: "",
+    en: "",
   },
   name: {
-    ru: 'Приемная комиссия',
-    kk: 'Қабылдау комиссиясы',
-    en: 'Admission Committee',
+    ru: "Приемная комиссия",
+    kk: "Қабылдау комиссиясы",
+    en: "Admission Committee",
   },
   home: {
     title: {
-      ru: 'Приемная комиссия',
-      kk: 'Қабылдау комиссиясы',
-      en: 'Admission Committee',
+      ru: "Главная страница",
+      kk: "Басты бет",
+      en: "Home page",
     },
-   description: {
-  ru: 'Главная страница официального сайта Приемной комиссии университета им. Шокана Уалиханова. Здесь вы найдете всю информацию о поступлении на бакалавриат, магистратуру и резидентуру, список образовательных программ, необходимые документы, сроки подачи заявок, новости университета и контакты. Ознакомьтесь с направлениями подготовки, специальностями и условиями обучения.',
-  kk: 'Шоқан Уәлиханов атындағы университеттің Қабылдау комиссиясының ресми беті. Мұнда бакалавриатқа, магистратураға және резидентураға түсу ережелері, білім беру бағдарламалары, қажет құжаттар, өтініш беру мерзімдері, университет жаңалықтары мен байланыс ақпараттары ұсынылған. Мамандықтар мен оқыту шарттары туралы біліңіз.',
-  en: 'Welcome to the official website of the Admission Committee of Shoqan Ualikhanov University. Learn everything about applying to bachelor\'s, master\'s, and residency programs. Explore the list of academic disciplines, required documents, application deadlines, admission rules, and contact details. Discover available majors and study conditions.',
-  },
+    description: {
+      ru: "Главная страница официального сайта Приемной комиссии университета им. Шокана Уалиханова. Здесь вы найдете всю информацию о поступлении на бакалавриат, магистратуру и резидентуру, список образовательных программ, необходимые документы, сроки подачи заявок, новости университета и контакты. Ознакомьтесь с направлениями подготовки, специальностями и условиями обучения.",
+      kk: "Шоқан Уәлиханов атындағы университеттің Қабылдау комиссиясының ресми беті. Мұнда бакалавриатқа, магистратураға және резидентураға түсу ережелері, білім беру бағдарламалары, қажет құжаттар, өтініш беру мерзімдері, университет жаңалықтары мен байланыс ақпараттары ұсынылған. Мамандықтар мен оқыту шарттары туралы біліңіз.",
+      en: "Welcome to the official website of the Admission Committee of Shoqan Ualikhanov University. Learn everything about applying to bachelor's, master's, and residency programs. Explore the list of academic disciplines, required documents, application deadlines, admission rules, and contact details. Discover available majors and study conditions.",
+    },
     keywords: {
-      ru: 'Главная страница официального сайта Приемной комиссии университета им. Шокана Уалиханова. Здесь вы найдете всю информацию о поступлении на бакалавриат, шокан уалиханов университет профессии, шокан уалиханов университет специальности, приемная комиссия шокан уалиханов, кокшетауский университет,шокан уалиханов университет кокшетау стоимость обучения 2025, шокан уалиханов университет кокшетау, магистратуру и резидентуру, список образовательных программ, необходимые документы, сроки подачи заявок, новости университета и контакты. Ознакомьтесь с направлениями подготовки, специальностями и условиями обучения.',
+      ru: "Главная страница официального сайта Приемной комиссии университета им. Шокана Уалиханова. Здесь вы найдете всю информацию о поступлении на бакалавриат, шокан уалиханов университет профессии, шокан уалиханов университет специальности, приемная комиссия шокан уалиханов, кокшетауский университет,шокан уалиханов университет кокшетау стоимость обучения 2025, шокан уалиханов университет кокшетау, магистратуру и резидентуру, список образовательных программ, необходимые документы, сроки подачи заявок, новости университета и контакты. Ознакомьтесь с направлениями подготовки, специальностями и условиями обучения.",
       kk: "Шоқан Уәлиханов атындағы университеттің қабылдау комиссиясының ресми сайтының басты беті. Мұнда сіз бакалавриатқа, магистратураға және резидентураға қабылдау туралы барлық ақпаратты таба аласыз: Шоқан Уәлиханов университетінің мамандықтары мен білім беру бағдарламалары, құжаттар тізімі, өтініш беру мерзімдері, оқу ақысы (Көкшетау, 2025), университет жаңалықтары және байланыс мәліметтері. Білім беру бағыттары мен оқу шарттарымен танысыңыз.",
-      en: "Homepage of the official website of the Admissions Committee of Shokan Ualikhanov University. Here you will find all the information about admission to bachelor's, master's, and residency programs: Shokan Ualikhanov University majors and specializations, academic programs, required documents, application deadlines, tuition fees (Kokshetau, 2025), university news, and contact information. Learn more about the fields of study and study conditions."
+      en: "Homepage of the official website of the Admissions Committee of Shokan Ualikhanov University. Here you will find all the information about admission to bachelor's, master's, and residency programs: Shokan Ualikhanov University majors and specializations, academic programs, required documents, application deadlines, tuition fees (Kokshetau, 2025), university news, and contact information. Learn more about the fields of study and study conditions.",
     },
   },
   education: {
     title: {
-      ru: 'Образовательные программы Шокан Уалиханов',
-      kk: 'Шоқан Уәлиханов университетінің білім беру бағдарламалары',
-      en: 'Educational Programs of Shoqan Ualikhanov University',
+      ru: "Образовательные программы Шокан Уалиханов",
+      kk: "Шоқан Уәлиханов университетінің білім беру бағдарламалары",
+      en: "Educational Programs of Shoqan Ualikhanov University",
     },
     description: {
-  ru: 'Страница образовательных программ университета имени Шокана Уалиханова. Ознакомьтесь с полным списком программ бакалавриата, магистратуры и резидентуры, направлениями подготовки, формами обучения и требованиями к поступающим. Здесь вы найдете актуальную информацию о специальностях и условиях обучения в университете.',
-  kk: 'Шоқан Уәлиханов атындағы университеттің білім беру бағдарламаларының беті. Мұнда бакалавриат, магистратура және резидентура бағдарламаларының толық тізімі, оқу бағыттары, оқыту түрлері және талапкерлерге қойылатын талаптар ұсынылған. Университет мамандықтары мен оқыту шарттары туралы толық ақпаратпен танысыңыз.',
-  en: 'Educational programs page of Shoqan Ualikhanov University. Explore the full list of bachelor\'s, master\'s, and residency programs, areas of study, learning formats, and admission requirements. Get detailed information about available majors and academic opportunities at the university.',
-},
+      ru: "Страница образовательных программ университета имени Шокана Уалиханова. Ознакомьтесь с полным списком программ бакалавриата, магистратуры и резидентуры, направлениями подготовки, формами обучения и требованиями к поступающим. Здесь вы найдете актуальную информацию о специальностях и условиях обучения в университете.",
+      kk: "Шоқан Уәлиханов атындағы университеттің білім беру бағдарламаларының беті. Мұнда бакалавриат, магистратура және резидентура бағдарламаларының толық тізімі, оқу бағыттары, оқыту түрлері және талапкерлерге қойылатын талаптар ұсынылған. Университет мамандықтары мен оқыту шарттары туралы толық ақпаратпен танысыңыз.",
+      en: "Educational programs page of Shoqan Ualikhanov University. Explore the full list of bachelor's, master's, and residency programs, areas of study, learning formats, and admission requirements. Get detailed information about available majors and academic opportunities at the university.",
+    },
     keywords: {
-      ru: 'Главная страница, новости, приемная комиссия, Шокана Уалиханова университет, список образовательных программ, профессии университета Шокана Уалиханова, Образовательные программы, Университет Шокана Уалиханова',
-      kk: 'Басты бет, жаңалықтар, Қабылдау комиссиясы, Шоқан Уәлиханов атындағы университет, білім беру бағдарламаларының тізімі, Шоқан Уәлиханов университетінің мамандықтары',
-      en: 'Home page, news, Admission Committee, Shoqan Ualikhanov University, list of educational programs, majors at Shoqan Ualikhanov University, educational programs, Shoqan Ualikhanov University',
+      ru: "Главная страница, новости, приемная комиссия, Шокана Уалиханова университет, список образовательных программ, профессии университета Шокана Уалиханова, Образовательные программы, Университет Шокана Уалиханова",
+      kk: "Басты бет, жаңалықтар, Қабылдау комиссиясы, Шоқан Уәлиханов атындағы университет, білім беру бағдарламаларының тізімі, Шоқан Уәлиханов университетінің мамандықтары",
+      en: "Home page, news, Admission Committee, Shoqan Ualikhanov University, list of educational programs, majors at Shoqan Ualikhanov University, educational programs, Shoqan Ualikhanov University",
     },
   },
-  news : {
-    title:{
-      ru: 'Новости',
-      kk: 'Жаңалықтар',
-      en: 'News',
-    },
-    description: {
-      ru: 'Новости и объявления: актуальная информация от приёмной комиссии Кокшетауского университета им. Ш. Уалиханова. Учебные программы, сроки подачи документов, гранты и важные обновления для абитуриентов.',
-      kk: 'Жаңалықтар мен хабарламалар: Ш. Уалиханова атындағы Көкшетау университетінің қабылдау комиссиясынан соңғы ақпараттар. Оқу бағдарламалары, құжат тапсыру мерзімдері, гранттар және талапкерлерге маңызды жаңартулар.',
-      en: 'News and announcements: Get the latest updates from the Admissions Committee of Shokan Ualikhanov Kokshetau University. Learn about academic programs, application deadlines, scholarships, and important information for prospective students.',
-    },
-    keywords: {
-  ru: 'Главная страница, новости, приемная комиссия, Шокана Уалиханова университет, список образовательных программ, профессии университета Шокана Уалиханова, Образовательные программы, Университет Шокана Уалиханова, поступление, гранты, сроки подачи документов, обучение в Казахстане, поступление в Кокшетау, абитуриентам',
-  kk: 'Басты бет, жаңалықтар, Қабылдау комиссиясы, Шоқан Уәлиханов атындағы университет, білім беру бағдарламаларының тізімі, Шоқан Уәлиханов университетінің мамандықтары, білім беру бағдарламалары, қабылдау, гранттар, құжат тапсыру мерзімдері, Қазақстанда оқу, Көкшетауға түсу, талапкерлерге',
-  en: 'Home page, news, Admission Committee, Shoqan Ualikhanov University, list of educational programs, majors at Shoqan Ualikhanov University, educational programs, Shoqan Ualikhanov University, admission, scholarships, application deadlines, study in Kazakhstan, apply to Kokshetau, prospective students'
-   },
-  }
-}
+};
