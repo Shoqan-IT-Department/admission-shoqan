b068355ed19e7d7a65e3f29705e028ee9519bc3d
donottookey <kirill123iop@gmail.com>
2026-06-12 13:48:25 +0500

    mod:ins.ts

--- client/src/config/instance.ts
+++ client/src/config/instance.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 // import { ENV } from '@/config/enviroments';
 
 export const ADM_URL = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://admission-api.shokan.edu.kz/api",
 });
